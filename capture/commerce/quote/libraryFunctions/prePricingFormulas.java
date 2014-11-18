19732547d7c8aa586f5fb6d708fbd920f8f72ce3
AaronQuintanilla <aaron.quintanilla@oracle.com>
2014-11-18 14:44:42 -0600

    Update to removal guardrails by quantity
    Removal guardrails are now multiplied by quantity.

--- capture/commerce/quote/libraryFunctions/prePricingFormulas.java
+++ capture/commerce/quote/libraryFunctions/prePricingFormulas.java
@@ -28,7 +28,7 @@ Updates:    20130913 - ??? - Added functionality to run large container pricing
                          for a division that has multiple InfoPro divs per Lawson div.
             20141027 - Julie Felberg - Passed ContainerGroupLine instead of containerGroup to GuardrailInputDict
             20141107 - Aaron Quintanilla - Removed price for Hand Pickup delivery
-			20141118 - Aaron Quintanilla - Set Removal Floor to 0.0
+			20141118 - Aaron Quintanilla - Set Removal Floor to 0.0 and to set removal guardrails by quantity
         
 =====================================================================================================
 */
@@ -1941,9 +1941,13 @@ for line in line_process{
                 if(containskey(guardrailOutputDict, "REM")){
                     //floorPriceStr = get(guardrailOutputDict, "REM");    //Changed 11/18/14 AQ
 					floorPriceStr = "0.0";    
+                    floorPriceStr = string(atof(floorPriceStr) * line._price_quantity);
                     basePriceStr = get(guardrailOutputDict, "REM");
+                    basePriceStr = string(atof(basePriceStr) * line._price_quantity);
                     targetPriceStr = get(guardrailOutputDict, "REM");
+                    targetPriceStr = string(atof(targetPriceStr) * line._price_quantity);
                     stretchPriceStr = get(guardrailOutputDict, "REM");
+                    stretchPriceStr = string(atof(stretchPriceStr) * line._price_quantity);
                 }
             }elif(rateTypeLower == "washout"){
                 if(containskey(guardrailOutputDict, "WAS")){
