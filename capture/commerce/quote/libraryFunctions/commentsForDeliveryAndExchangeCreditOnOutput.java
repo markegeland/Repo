917edbd1a8713f21885cfe31852c4b21d96702ca
Gaurav Dawar <gdawar@republicservices.com>
2015-01-22 09:37:01 -0700

    #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal

--- capture/commerce/quote/libraryFunctions/commentsForDeliveryAndExchangeCreditOnOutput.java
+++ capture/commerce/quote/libraryFunctions/commentsForDeliveryAndExchangeCreditOnOutput.java
@@ -1,8 +1,34 @@
+/*
+=======================================================================================================================
+Name:   Print Action - comments For Delivery And Exchange Credit On Output
+Author:   
+Create date:  
+
+Description: Runs code on print and email actions. Performs complex functionality to generate string values to be used in DOC engine
+        
+Input:       line item grid values.            
+                    
+Output:      String (documentNumber + "~" + attributeVariableName + "~" + value + "|")
+
+Updates:   	 01/21/15 - Gaurav (Republic) - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+
+
+=======================================================================================================================
+*/
 retStr = "";
 creditStr = "";
 installStr = "";
 pickupStr = "";
 minTonStr = "";
+//line 24 to 31 - added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+container_count = "";
+numberOfTotalRemovalContainers = 0;
+numberOfTotalDeliveryContainers = 0;
+exchangeChargeSubtotal_onetime = 0.0;
+deliveryChargeSubtotal_onetime = 0.0;
+remCredit2 = 0.0;
+deliveryCredit2 = 0.0;
+divisionDelStr_ui = 0.0;
 modelDocNumArr = string[];
 routeTypeDict = dict("string"); 
 sizeDict = dict("string");
@@ -60,9 +86,28 @@ for line in line_process{
 			containerSize = get(sizeDict, parentDocNum);
 		}
 		if(line.rateType_line == "Delivery"){
-			deliveryCredit = line.totalTargetPrice_line -  line.sellPrice_line ;
+			//line 90 to 112 - added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+			if(line._price_quantity > 1){
+				container_count = "containers";
+			}else{
+				container_count = "container";
+			}
+			numberOfTotalDeliveryContainers = line._price_quantity;
+			deliveryCredit = (line.totalTargetPrice_line -  line.sellPrice_line) *  line._price_quantity;
+			parentDoc = line._parent_doc_number;
 			if(deliveryCredit > 0.0){
-				creditStr = creditStr + "<p>" + "Valued Customer Discount - Delivery for " + routeType + " " + containerSize + " yard - " + formatascurrency(deliveryCredit, "USD") + "</p>";				
+				deliveryCredit2 = deliveryCredit2 + (line.totalTargetPrice_line -  line.sellPrice_line) *  line._price_quantity;
+				deliveryChargeSubtotal_onetime = deliveryChargeSubtotal_onetime + line.totalTargetPrice_line * line._price_quantity;
+				print "total";print deliveryChargeSubtotal_onetime;
+				print "deliveryCredit2";print deliveryCredit2;
+				divisionDelStr_ui = line.totalTargetPrice_line;
+				creditStr = creditStr + "<p>" + "Valued Customer Discount - Delivery for " + string(numberOfTotalDeliveryContainers) + " " + container_count + " " + routeType + " " + containerSize + " yard - " + formatascurrency(deliveryCredit, "USD") + "</p>";	
+				retStr = retStr + parentDoc + "~" + "divisionDelivery_ui_line" + "~" + string(divisionDelStr_ui) + "|";
+			}
+			else{
+				deliveryChargeSubtotal_onetime = deliveryChargeSubtotal_onetime + line.sellPrice_line * line._price_quantity;
+				divisionDelStr_ui = line.sellPrice_line;
+				retStr = retStr + parentDoc + "~" + "divisionDelivery_ui_line" + "~" + string(divisionDelStr_ui) + "|";
 			}
 		}
 		if(line.rateType_line == "Exchange"){
@@ -72,9 +117,21 @@ for line in line_process{
 			}
 		}
 		if(line.rateType_line == "Removal"){
-			remCredit = line.totalTargetPrice_line -  line.sellPrice_line ;
+			//line 121 to 136 - added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+			if(line._price_quantity > 1){
+				container_count = "containers";
+			}else{
+				container_count = "container";
+			}
+			numberOfTotalRemovalContainers = line._price_quantity;
+			remCredit = (line.totalTargetPrice_line -  line.sellPrice_line) *  line._price_quantity;
 			if(remCredit > 0.0){
-				creditStr = creditStr + "<p>" + "Valued Customer Discount - Removal for " + routeType + " " + getconfigattrvalue(line._parent_doc_number,"containerSize_readOnly") + " yard - " + formatascurrency(remCredit, "USD") + "</p>";
+				remCredit2 = remCredit2 + (line.totalTargetPrice_line -  line.sellPrice_line) *  line._price_quantity;
+				exchangeChargeSubtotal_onetime = exchangeChargeSubtotal_onetime + line.totalTargetPrice_line * line._price_quantity;
+				creditStr = creditStr + "<p>" + "Valued Customer Discount - Removal for " + string(numberOfTotalRemovalContainers) + " " + container_count + " " + routeType + " " + getconfigattrvalue(line._parent_doc_number,"containerSize_readOnly") + " yard - " + formatascurrency(remCredit, "USD") + "</p>";
+			}
+			else{
+				exchangeChargeSubtotal_onetime = exchangeChargeSubtotal_onetime + line.sellPrice_line * line._price_quantity;
 			}
 		}
 		if(line.rateType_line == "Installation"){
@@ -105,5 +162,17 @@ retStr = retStr + "1~" + "deliveryAndExchangeCreditStringForOutput_quote" + "~" 
 retStr = retStr + "1~" + "installationChargeHTMLString_quote" + "~" + installStr + "|";
 retStr = retStr + "1~" + "pickUpDaysCommentHTML_quote" + "~" + pickupStr + "|";
 retStr = retStr + "1~" + "minimumTonnageHTMLStr_quote" + "~" + minTonStr + "|";
+//added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+retStr = retStr + "1~" + "oneTime_exchangeCustomerDiscount_quote" + "~" + string(remCredit2) + "|";
+//added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+retStr = retStr + "1~" + "numberOfTotalRemovalContainers_quote" + "~" + string(numberOfTotalRemovalContainers) + "|";
+//added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+retStr = retStr + "1~" + "numberOfTotalDeliveryContainers_quote" + "~" + string(numberOfTotalDeliveryContainers) + "|";
+//added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+retStr = retStr + "1~" + "oneTime_valuedCustomerDiscount_quote" + "~" + string(deliveryCredit2) + "|";
+//added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+retStr = retStr + "1~" + "exchangeChargeSubtotal_onetime_quote" + "~" + string(exchangeChargeSubtotal_onetime) + "|";
+//added 20150119 - GD - #352 #322 - making delivery and removal "Per Service compared to "One time" and correcting the calculations for Delivery and removal
+retStr = retStr + "1~" + "deliveryChargeSubtotal_onetime_quote" + "~" + string(deliveryChargeSubtotal_onetime) + "|";
 
 return retStr;
