91c0f8fd4fce2f1e34aaf0d2fe2a4ada026161a6
John Palubinskas <john@azsoftwaredev.com>
2015-02-04 15:57:56 -0700

    #387 add saleFinalizedDate_quote attribute

--- capture/commerce/quote/actions/finalizeContract_quote.after.java
+++ capture/commerce/quote/actions/finalizeContract_quote.after.java
@@ -1,11 +1,36 @@
-retStr = "";
-retStr = retStr +  "1~isSaleFinalized_quote~true|" +commerce.printing() + commerce.setStatus("finalizeContract") + commerce.setTransactionCode() + commerce.unconfiguredServicesString();
-retStr = retStr + "1~saleFinalizedBy_quote~" + _system_user_first_name + " "  + _system_user_last_name + "|";
+/* 
+================================================================================
+       Name:  finalizeContract_quote - After action
+     Author:  ???
+Create date:  ???
+Description:  Runs after the finalizeContract action fires
+        
+  Input:  none
+                    
+ Output:  String (documentNumber + "~" + attributeVariableName + "~" + value + "|")
+
+Updates:  20150204 - John Palubinskas - #387 Added saleFinalizedDate_quote functionality.
+
+================================================================================
+*/
+
+res = "";
+
 //Comments section on CSA needs only the letter in Lead Source Code
 leadCodeOnCSA = "";
 if(leadSourceCode_quote <> ""){
     leadSouceCodeArr = split(leadSourceCode_quote, " - ");
     leadCodeOnCSA = leadSouceCodeArr[0];
 }
-retStr = retStr + "1~leadSourceCodeOnCSA_quote~" + leadCodeOnCSA + "|";
-return retStr;
+
+res = res
+        + "1~isSaleFinalized_quote~true|"
+        + "1~saleFinalizedBy_quote~" + _system_user_first_name + " "  + _system_user_last_name + "|"
+        + "1~leadSourceCodeOnCSA_quote~" + leadCodeOnCSA + "|"
+        + "1~saleFinalizedDate_quote~" + datetostr(getDate(),"MM/dd/yyyy HH:mm:ss") + "|"
+        + commerce.printing()
+        + commerce.setStatus("finalizeContract")
+        + commerce.setTransactionCode()
+        + commerce.unconfiguredServicesString();
+
+return res;
