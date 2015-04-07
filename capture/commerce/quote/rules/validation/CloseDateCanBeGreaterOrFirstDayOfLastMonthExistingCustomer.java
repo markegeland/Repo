fec3cd2a230894d649b724b047c59136590e838d
Gaurav Dawar <gdawar@republicservices.com>
2015-04-07 10:36:32 -0700

    #59 - Removed the close date error for non change of owner quotes.

--- capture/commerce/quote/rules/validation/CloseDateCanBeGreaterOrFirstDayOfLastMonthExistingCustomer.java
+++ capture/commerce/quote/rules/validation/CloseDateCanBeGreaterOrFirstDayOfLastMonthExistingCustomer.java
@@ -14,7 +14,7 @@ else{
 newDate=temp[0]+dateSeperator+"01"+dateSeperator+temp[2];
 firstDayOfthisMonth=strtodate(newDate, dateFormat);
 lastdayOfMonthBefore=adddays(firstDayOfthisMonth,-1);
-if(_system_current_step_var == "generateDocuments" AND chooseCSA_quote){
+if(_system_current_step_var == "generateDocuments" AND chooseCSA_quote AND salesActivity_quote == "Change of Owner"){
 	if(serviceCloseDate_quote<> "" AND comparedates(formattedServiceCloseDate ,lastdayOfMonthBefore) == -1){
 		return true;
 	}
