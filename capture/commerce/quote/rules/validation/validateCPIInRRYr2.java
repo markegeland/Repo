5561cbf312f34aaffd671e56bee5f68e7011e723
Gaurav Dawar <gdawar@republicservices.com>
2015-06-09 11:50:42 -0700

    #325 Value "0.0" was added to allowed values.

--- capture/commerce/quote/rules/validation/validateCPIInRRYr2.java
+++ capture/commerce/quote/rules/validation/validateCPIInRRYr2.java
@@ -1,9 +1,10 @@
 /*
 20150528 - Gaurav Dawar - #325 This Rule was created to validate that user is not able to put anything in "after year2" text boxes other than float numbers and the word "CPI".
+20150609 - Gaurav Dawar - #325 Value "0.0" was added to allowed values.
 */
 yr_2_text = year2Rate_quote;
 if(customerRateRestriction_quote == True){
-	if(yr_2_text == "" OR (isnumber(yr_2_text)==True AND atof(yr_2_text) > 0.0) OR yr_2_text == "CPI"){
+	if(yr_2_text == "" OR (isnumber(yr_2_text)==True AND atof(yr_2_text) >= 0.0) OR yr_2_text == "CPI"){
 		return false;
 	}
 	return true;
--- capture/commerce/quote/rules/validation/validateCPIInRRYr3.java
+++ capture/commerce/quote/rules/validation/validateCPIInRRYr3.java
@@ -1,9 +1,10 @@
 /*
 20150528 - Gaurav Dawar - #325 This Rule was created to validate that user is not able to put anything in "after year3" text boxes other than float numbers and the word "CPI".
+20150609 - Gaurav Dawar - #325 Value "0.0" was added to allowed values.
 */
 yr_3_text = year3Rate_quote;
 if(customerRateRestriction_quote == True){
-	if(yr_3_text == "" OR (isnumber(yr_3_text)==True AND atof(yr_3_text) > 0.0) OR yr_3_text == "CPI"){
+	if(yr_3_text == "" OR (isnumber(yr_3_text)==True AND atof(yr_3_text) >= 0.0) OR yr_3_text == "CPI"){
 		return false;
 	}
 	return true;
